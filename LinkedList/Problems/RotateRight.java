18099a50eefb338656660e0f299f6303abe99b70
RupamManna8 <rupammanna140@gmail.com>
2026-08-16 22:33:33 +0530

    Rotate Right List problem

---
+++ LinkedList/Problems/RotateRight.java
@@ -0,0 +1,35 @@
+package LinkedList.Problems;
+
+import LinkedList.Problems.AddToNumber.ListNode;
+
+public class RotateRight {
+    public ListNode rotateRight(ListNode head, int k) {
+        if (head == null || head.next == null || k == 0) {
+            return head;
+        }
+
+        int length = 1;
+        ListNode tail = head;
+        while (tail.next != null) {
+            tail = tail.next;
+            length++;
+        }
+
+        k = k % length;
+        if (k == 0)
+            return head;
+
+        tail.next = head;
+
+        int stepsToNewTail = length - k;
+        ListNode newTail = tail;
+        while (stepsToNewTail-- > 0) {
+            newTail = newTail.next;
+        }
+
+        ListNode newHead = newTail.next;
+        newTail.next = null;
+
+        return newHead;
+    }
+}
