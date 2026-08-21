bfec2bd36c3db4a53696a6ef296acbb93c613e16
RupamManna8 <rupammanna140@gmail.com>
2026-08-21 23:45:02 +0530

    optimized

--- ArrayStrings/Day23/SearchIn2d.java
+++ ArrayStrings/Day23/SearchIn2d.java
@@ -1,7 +1,8 @@
 package ArrayStrings.Day23;
 
 public class SearchIn2d {
-    public boolean BinarySearch(int arr[],int target){
+    public static int count = 0;
+    public static boolean BinarySearch(int arr[],int target){
         int left = 0;
         int right = arr.length - 1;
         while (left <= right) {
@@ -14,15 +15,33 @@ public class SearchIn2d {
             }else{
                 right = mid - 1;
             }
+            count++;
         }
         return false;
     }
-    public boolean searchMatrix(int[][] matrix, int target) {
-        for(int[] i : matrix){
-            if(i[0] <= target && target <= i[i.length - 1]){
-                return BinarySearch(i, target);
+    public static boolean searchMatrix(int[][] matrix, int target) {
+        int left = 0;
+        int right = matrix.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            int check = matrix[mid][matrix[mid].length -1];
+            if(target >= matrix[mid][0] && target <= check){
+                return BinarySearch(matrix[mid], target);
             }
+            if(target > check){
+                left = mid + 1;
+            }else{
+                right = mid - 1;
+            }
+            count++;
         }
         return false;
+    }
+
+    public static void main(String[] args) {
+      int  [][] matrix = {{1,3,5,7},{10,11,16,20},{23,30,34,60}}; 
+      int target = 11;
+      System.out.println(searchMatrix(matrix, target));
+      System.out.println("count:" + count);
     }
 }
