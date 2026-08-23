cde204f05b9e4b04e80e7572b6cb1ed9ec888426
RupamManna8 <rupammanna140@gmail.com>
2026-08-23 21:07:25 +0530

    day 25

---
+++ ArrayStrings/Day25/BestTimetoBuyStock.java
@@ -0,0 +1,14 @@
+package ArrayStrings.Day25;
+
+public class BestTimetoBuyStock {
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+        for(int i = 1;i< prices.length;i++){
+            if(prices[i - 1] >= prices[i] ){
+                continue;
+            }
+            profit += prices[i] - prices[i - 1];
+        }
+        return profit;
+    }
+}
