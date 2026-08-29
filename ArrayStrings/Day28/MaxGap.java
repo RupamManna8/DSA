31c0ed4a6ba5dbdf1508a16e360c93f00d7797e3
RupamManna8 <rupammanna140@gmail.com>
2026-08-29 20:39:54 +0530

    day28

---
+++ ArrayStrings/Day28/MaxGap.java
@@ -0,0 +1,21 @@
+package ArrayStrings.Day28;
+
+import java.util.Arrays;
+
+public class MaxGap {
+    public int maximumGap(int[] nums) {
+        if (nums == null || nums.length < 2) {
+            return 0;
+        }
+
+        Arrays.sort(nums);
+
+        int maxGap = 0;
+
+        for (int i = 1; i < nums.length; i++) {
+            maxGap = Math.max(nums[i] - nums[i - 1], maxGap);
+        }
+
+        return maxGap;
+    }
+}
